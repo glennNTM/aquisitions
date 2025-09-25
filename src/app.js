d0dcb3565506136ad35b555b97cf2fadd1c3ad40
unknown <glennntoutoume8@gmail.com>
2025-09-25 10:59:16 +0100

    Implementation de ESlint et Prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
-import express from 'express'
+import express from 'express';
 
 // Creation de l'application Express
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Salut Aquisitions!')
+  res.status(200).send('Salut Aquisitions!');
+});
 
-})
-
-export default app
+export default app;
